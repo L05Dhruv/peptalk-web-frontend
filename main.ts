8f7ba99c8c214e50c3a34521eff32c7365ccc32a
Dhruv Thota <dhruv@Mac.lan>
2025-01-09 19:22:32 -0500

    fixed about page

--- main.ts
+++ main.ts
@@ -85,6 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get Started page submit Button
     const getStartedButton: HTMLElement | null = document.querySelector('.submit-button');
     getStartedButton?.addEventListener('click', function() {
+        
         console.log('button clicked');
         if (!getStartedButton) return;
 
